408b0b6cc9f15fdd5b90938f037aa0fb298a45e7
NimrahAltafAdam <nimrahadam.123@gmail.com>
2022-03-13 13:03:47 -0700

    HeroSection

--- src/components/Navbar/NavbarElements.js
+++ src/components/Navbar/NavbarElements.js
@@ -7,7 +7,7 @@ export const Nav = styled.nav`
 background: #000;
 /*background: transparent;*/
 height: 80px;
-/*margin-top: -80px;*/
+margin-top: -80px;
 display: flex;
 justify-content: center;
 font-size: 1rem;
